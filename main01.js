f732fe7842bf0670542ef982d8b307df5a761bbb
bipaflo <48183752+bipaflo@users.noreply.github.com>
2019-03-29 16:53:31 +0100

    Add files via upload

---
+++ main01.js
@@ -0,0 +1,14 @@
+setTimeout(func, 10000);
+setTimeout(func, 2000);
+function func() {
+    alert('Veux tu visiter xxxvidsxxx?? Il est génial je te le conseille vivement ! ');
+    if(confirm('Veux tu visiter xxxvidsxxx?? Il est génial je te le conseille vivement ! ')){
+
+    	window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+
+    }
+
+	else {
+
+	}
+}
